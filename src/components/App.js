f12992a678393633cd5e7fbefbdb7af7f48c9eaa
vishal pawar <pawarvishal29999@gmail.com>
2023-10-01 11:09:55 +0530

    commit-tap-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
 import React from "react";
 import './../styles/App.css';
+import Tabs from './Tabs';
 
 const App = () => {
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: <p>This is the content for Tab 1.</p>,
+    },
+    {
+      title: 'Tab 2',
+      content: <p>This is the content for Tab 2.</p>,
+    },
+    {
+      title: 'Tab 3',
+      content: <p>This is the content for Tab 3.</p>,
+    },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Tabs Example</h1>
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
